fcd9412e6ad434aa50e22c45ada0e0a439232a41
Abdullah Hafeez <123733124+abdullahafeez@users.noreply.github.com>
2023-02-26 18:53:52 +0500

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -5,7 +5,7 @@ def buildJar() {
 
 def buildImage() {
     echo "building the docker image..."
-    withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+    withCredentials([usernamePassword(credentialsId: 'dockerhub-id', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
         sh 'docker build -t abdullahhafeez/test-repo:jma-1.0 .'
         sh "echo $PASS | docker login -u $USER --password-stdin"
         sh 'docker push abdullahhafeez/test-repo:jma-1.0'
